e63d774d7f78ac81d672cf03761b703ff94af5ba
Maarten <maarten@v-ittersum.nl>
2015-12-23 18:42:50 +0100

    Added a super class for ClientStore so EntityStore can be made

--- src/MinecraftClientStore.js
+++ src/MinecraftClientStore.js
@@ -1,12 +1,9 @@
 "use strict"
+const MinecraftStore = require('./MinecraftStorePrototype')
 
-class MinecraftClientStore {
-  constructor() {
-    this._clients = {}
-    this._listening = {}
-  }
+class MinecraftClientStore extends MinecraftStore {
   find(query = {}) {
-    var clients = this._clients
+    var clients = this._stored
     var list = Object.keys(clients).map(k => clients[k])
     if(query.id !== undefined)
       list.forEach((client, key) => {if(client.id !== query.id) list.splice(key, 1)})
@@ -17,36 +14,12 @@ class MinecraftClientStore {
     return list
   }
   add(id, client) {
-    if(this._clients[id]) return false;
-    if(this.find({uuid: client.uuid}))
-    this._clients[id] = client
     var self = this
-    client.on('disconnected', () => delete self._clients[id])
-    for(var eventName in this._listening)
-      if(this._listening.hasOwnProperty(eventName)) this._bindListener(client, eventName);
-  }
-  get all() {
-    return this._clients
-  }
-  get array() {
-    var clients = this._clients
-    return Object.keys(clients).map(k => clients[k])
-  }
-  get length() {
-    return Object.keys(this._clients).length
-  }
-  on(eventName, executer) {
-    if(!this._listening[eventName]) this._listening[eventName] = [executer]
-    else this._listening.push(executer)
-  }
-  forEach(callback) {
-    for(var k in this._clients) {
-      if(!this._clients.hasOwnProperty(k)) continue;
-      callback(this._clients[k])
-    }
+    if(!super.add(id, client)) return false
+    client.on('disconnected', () => delete self._stored[id])
   }
   others(client) {
-    var clients = this._clients
+    var clients = this._stored
     return callback => {
       for(var k in clients) {
         if(!clients.hasOwnProperty(k)) continue;
@@ -55,16 +28,13 @@ class MinecraftClientStore {
       }
     }
   }
-
-  _bindListener(client, event) {
-    var listeners = this._listening[event]
-    client.on(event, function() {
-      var eventArgs = [].slice.apply(arguments)
-      listeners.forEach(executer => {
-        var args = [client]
-        eventArgs.forEach(arg => args.push(arg))
-        executer.apply(null, args)
-      })
+  inRange(pos, range, callback) {
+    this.all(cl => {
+      if((cl.pos.x - range) > pos.x) return
+      if((cl.pos.x + range) < pos.x) return
+      if((cl.pos.y - range) > pos.y) return
+      if((cl.pos.y + range) < pos.y) return
+      callback(cl)
     })
   }
 }
