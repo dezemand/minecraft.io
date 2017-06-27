65fdd51b8875a7d4b7a9df77e046b8762398b57f
Maarten van Ittersum <maarten@v-ittersum.nl>
2017-06-27 20:24:22 +0200

    Changed 2 properties from public to readonly

--- src/Minecraft/ChatCommand.ts
+++ src/Minecraft/ChatCommand.ts
@@ -1,23 +1,20 @@
 "use strict"
 
 export default class MinecraftChatCommand {
-  constructor(public raw: string) {}
+  constructor (readonly raw: string) {
+  }
 
-  name = this.raw.split(' ')[0].substr(1)
+  readonly name = this.raw.split(' ')[0].substr(1)
 
-  getArgs(length: number = 1): Array<string> {
+  public getArgs (length: number = 1): Array<string> {
     if(length < 1)
       length = 1
-
     if(this.raw.indexOf(' ') === -1)
       return null
-
     const args: Array<string> = this.raw.split(' ')
     args.splice(0, 1)
-
     const last: Array<string> = args.splice(length - 1, args.length - length + 1)
     args.push(last.join(' '))
-
     return args
   }
 }
